89b12cbe0fec99dea22b2a858cf66ef155d4516d
hiphoox <norberto@bunsan.io>
2019-04-28 19:06:40 -0500

    Refactoring function order and fixing error handling

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ use std::io::{self, Write};
 use std::process::{Command, Stdio};
 use std::str::Split;
 
+pub fn print_shell_prompt() {
+    print!("-> ");
+    io::stdout().flush().unwrap();
+}
+
 pub fn read_commands() -> String {
     let mut buffer = String::new();
     io::stdin()
@@ -10,29 +15,27 @@ pub fn read_commands() -> String {
     buffer
 }
 
-pub fn parse_command(result: &str) -> Vec<&str> {
-    let parsed_result: Vec<&str> = result.split_whitespace().collect();
-    parsed_result
-}
-
 pub fn split_commands(result: &String) -> Split<&str> {
-    let parsed_result = result.split(";");
-    parsed_result
+    result.split(";")
 }
 
 pub fn execute_commands(commands: Split<&str>) {
     for raw_command in commands {
         let command = parse_command(raw_command);
-        Command::new(command[0])
+        let output = Command::new(command[0])
             .env("PATH", "/bin")
             .args(&command[1..])
             .stdout(Stdio::inherit())
-            .output()
-            .expect("failed");
+            .stderr(Stdio::inherit())
+            .output();
+        if output.is_err() {
+            let error = output.unwrap_err();
+            print!("{}\n", error);
+        }
     }
 }
 
-pub fn print_shell_prompt() {
-    print!("-> ");
-    io::stdout().flush().unwrap();
+pub fn parse_command(result: &str) -> Vec<&str> {
+    let parsed_result: Vec<&str> = result.split_whitespace().collect();
+    parsed_result
 }
